8c3a3a047fe3a46e265946372a01e6f46068bf18
maria <maria.shubenkina@yandex.ru>
2024-02-02 18:00:17 +0300

    add_app

---
+++ README.md
@@ -0,0 +1,2 @@
+### test app fast_api async/sync ###
+
---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+"""для инициализации объектов"""
+
+from .base import Base
+from .user import User
+from .author import Author
+from .post import Post
+from .tag import Tag
